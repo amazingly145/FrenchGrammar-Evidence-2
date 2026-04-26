bbbb80e45314e6afe660b11997bad6183685cd19
amazingly145 <A01753419@tec.mx>
2026-04-26 14:55:27 -0600

    Eliminando ambiguedad ya sea en enunciados simples o compuestos

--- french_grammar.py
+++ french_grammar.py
@@ -2,7 +2,7 @@ import nltk
 from nltk import CFG
 
 
-# Grammar with ambiguery and left recursion
+# Grammar with ambiguety and left recursion
 grammar = CFG.fromstring("""
     S -> NP
     NP -> Pron B | A
@@ -32,6 +32,50 @@ for tree in trees:
 
 
 # Grammar without ambiguety and left recursion
+print("Gramatica sin ambiguedad")
+print("Primera iteración para quitar la ambiguedad")
+print("Se quita la ambiguedad en enunciados simples, pero no compuestas")
+grammar = CFG.fromstring("""
+S -> NP
+    NP -> Pron B | A
+    A -> D
+    D -> NP E
+    E -> CONJ NP
+    B -> Verb C
+    C -> Det COD
+
+    Pron -> 'Je' | 'Tu' | 'Il' | 'Elle' | 'Nous' | 'Vous' | 'Ils' | 'Elles'
+    CONJ -> 'et' | 'ou'
+    Verb -> 'etre' | 'avoir' | 'mange' | 'regarde' | 'parler' | 'aime'
+    COD -> 'film' | 'pomme' | 'musique' | 'orange' | 'pizza' | 'peinture' | 'maison'
+    Det -> 'le' | 'la' | 'une' | 'un'
+""")
+parser = nltk.ChartParser(grammar)
+
+print("Enunciado uno")
+sentence = "Je mange une pomme"
+tokens = sentence.split()
+
+trees = list(parser.parse(tokens))
+
+print("arboles:", len(trees))
+
+for tree in trees:
+    tree.pretty_print()
+
+print("Enunciado 2")
+sentence = "Je mange une pomme et Tu aime la musique ou Il regarde le film"
+tokens = sentence.split()
+
+trees = list(parser.parse(tokens))
+
+print("arboles:", len(trees))
+
+for tree in trees:
+    tree.pretty_print()
+
+print("Segunda iteración para quitar la ambiguedad")
+print("Se quita la ambiguedad en enunciados simples y compuestas")
 grammar = CFG.fromstring("""
     S -> NP
     NP -> Pron B | A
@@ -50,7 +94,18 @@ grammar = CFG.fromstring("""
 """)
 parser = nltk.ChartParser(grammar)
 
-print("Gramatica sin ambiguedad")
+print("Enunciado uno")
+sentence = "Je mange une pomme"
+tokens = sentence.split()
+
+trees = list(parser.parse(tokens))
+
+print("arboles:", len(trees))
+
+for tree in trees:
+    tree.pretty_print()
+
+print("Enunciado 2")
 sentence = "Je mange une pomme et Tu aime la musique ou Il regarde le film"
 tokens = sentence.split()
 
