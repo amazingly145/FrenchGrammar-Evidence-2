d68e6836d8e1a146b7329a6fcc29786c2b100897
amazingly145 <A01753419@tec.mx>
2026-04-26 15:31:02 -0600

    Quitar la recursividad a la izquierda

--- french_grammar.py
+++ french_grammar.py
@@ -78,9 +78,49 @@ print("Segunda iteración para quitar la ambiguedad")
 print("Se quita la ambiguedad en enunciados simples y compuestas")
 grammar = CFG.fromstring("""
     S -> NP
-    NP -> Pron B | A
-    A -> D
-    D -> NP E
+    NP -> Pron B | NP E
+    E -> CONJ F
+    F -> Pron B
+    B -> Verb C
+    C -> Det COD
+
+    Pron -> 'Je' | 'Tu' | 'Il' | 'Elle' | 'Nous' | 'Vous' | 'Ils' | 'Elles'
+    CONJ -> 'et' | 'ou'
+    Verb -> 'etre' | 'avoir' | 'mange' | 'regarde' | 'parler' | 'aime'
+    COD -> 'film' | 'pomme' | 'musique' | 'orange' | 'pizza' | 'peinture' | 'maison'
+    Det -> 'le' | 'la' | 'une' | 'un'
+""")
+parser = nltk.ChartParser(grammar)
+
+print("Enunciado uno")
+sentence = "Je mange une pomme"
+tokens = sentence.split()
+
+trees = list(parser.parse(tokens))
+
+print("arboles:", len(trees))
+
+for tree in trees:
+    tree.pretty_print()
+
+print("Enunciado 2")
+sentence = "Je mange une pomme et Tu aime la musique ou Il regarde le film"
+tokens = sentence.split()
+
+trees = list(parser.parse(tokens))
+
+print("arboles:", len(trees))
+
+for tree in trees:
+    tree.pretty_print()
+
+# Eliminamos la recurisividad a la izquierda
+print("Gramatica sin ambiguedad y recursividad a la izquierda")
+#NPP es NP'
+grammar = CFG.fromstring("""
+    S -> NP
+    NP -> Pron B NPP 
+    NPP -> E NPP | 
     E -> CONJ F
     F -> Pron B
     B -> Verb C
